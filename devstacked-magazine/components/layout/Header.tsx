68f4d96dbb16894758f92fce762e7752cfced1e4
FatlumG <fatlumg03@gmail.com>
2026-02-11 13:27:41 +0100

    Added stats

--- devstacked-magazine/components/layout/Header.tsx
+++ devstacked-magazine/components/layout/Header.tsx
@@ -1,12 +1,15 @@
 import Navigation from "../layout/Navigation";
+import Logo from '@/public/logos/devstacked-horizontally.svg'
 import Link from "next/link";
 import Button from "../ui/Button";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center h-28 text-white px-[100px]">
       <div className="">
-        <Link href="/">Logo</Link>
+        <Image src={Logo} alt="Logo" className="scale-150 mt-3" />
+        {/* <Link href="/">Logo</Link> */}
       </div>
       <Navigation />
       <div>
