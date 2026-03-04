af0c397ca3c016bb3d40034c64ece7ea692ba84d
LaurentMaxhuni <152623037+LaurentMaxhuni@users.noreply.github.com>
2026-03-04 01:03:07 +0100

    Minor UI tweaks: Some items weren't centered and it didn't appear visually attractive.

--- components/contact/Info.tsx
+++ components/contact/Info.tsx
@@ -12,33 +12,33 @@ function Info() {
       </p>
       <div className='mt-8 flex flex-col gap-3 sm:mt-10'>
         <h2 className="mb-3 mt-6 text-xl font-semibold text-white sm:mb-4 sm:mt-8 sm:text-2xl">Contact Information</h2>
-        <div className='flex items-start gap-3 text-white'>
+        <div className='flex items-center gap-3 text-white'>
           <div className='flex shrink-0 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-600 p-2'>
             <MailIcon />
           </div>
-          <p className="text-gray-300 break-words">
+          <p className="text-gray-300 wrap-break-word">
             Email:
             <a href="mailto:devstackedmagazine@gmail.com" className="ml-1 break-all text-red-active hover:underline">
               devstackedmagazine@gmail.com
             </a>
           </p>
         </div>
-        <div className='flex items-start gap-3 text-white'>
+        <div className='flex items-center gap-3 text-white'>
           <div className='flex shrink-0 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-600 p-2'>
             <Instagram />
           </div>
-          <p className="text-gray-300 break-words">
+          <p className="text-gray-300 wrap-break-word">
             Instagram:
             <a href="https://www.instagram.com/devstackedmagazine" target="_blank" className="ml-1 break-all text-red-active hover:underline">
               @devstackedmagazine
             </a>
           </p>
         </div>
-        <div className='flex items-start gap-3 text-white'>
+        <div className='flex items-center gap-3 text-white'>
           <div className='flex shrink-0 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-600 p-2'>
             <Music2 />
           </div>
-          <p className="text-gray-300 break-words">
+          <p className="text-gray-300 wrap-break-word">
             Tiktok:
             <a href="https://www.tiktok.com/@devstackedmagazine" target="_blank" className="ml-1 break-all text-red-active hover:underline">
               @devstackedmagazine
