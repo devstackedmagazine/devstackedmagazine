a61677879a1fc03bf9698da818dbf932077b9cfc
FatlumG <fatlumg03@gmail.com>
2026-04-10 22:00:24 +0200

    Fixing

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Navigation from "../layout/Navigation";
+// @ts-ignore
 import Logo from "@/public/logos/devstacked-horizontally.svg";
 import Button from "../ui/Button";
+import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { variants } from "@/lib/motion-presets";
